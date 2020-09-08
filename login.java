0c87865d75e7b75f25b291101af81add2e2a956c
Oscar <42092518+Webatron11@users.noreply.github.com>
2020-09-08 14:05:21 +0200

    Syncing offline and online. (#7)
    * Create login.java
    
    * Create passHash.java

---
+++ login.java
@@ -0,0 +1,6 @@
+public class login {
+//https://dev.to/awwsmm/how-to-encrypt-a-password-in-java-42dh
+  public static void main(String[] args) {
+  
+  }
+}
---
+++ passHash.java
@@ -0,0 +1,6 @@
+public class passHash {
+//https://dev.to/awwsmm/how-to-encrypt-a-password-in-java-42dh
+  public static void main(String[] args) {
+  
+  }
+}
