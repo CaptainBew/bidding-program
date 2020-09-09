19edbfba7f2c4b3590d93650ff176718fad92437
Webatron11 <42092518+Webatron11@users.noreply.github.com>
2020-09-09 20:37:01 +0200

    Starting on login.java

--- cmds/login.java
+++ cmds/login.java
@@ -1,8 +1,12 @@
 package cmds;
 
+
+
 public class login {
 //https://dev.to/awwsmm/how-to-encrypt-a-password-in-java-42dh
   public static void main(String[] args) {
-  
+    MongoClientURI dbToken = new MongoClientURI("mongodb+srv://OWPC:<password>@bidding.92wgv.mongodb.net/<dbname>?retryWrites=true&w=majority");
+    MongoClient dbClient = new MongoClient(dbToken);
+    MongoDatabase db = dbClient.getDatabase("test");
   }
 }
